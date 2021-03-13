ffc516bb09dcaf18e4a92e7aac5b8b88484a1894
Эрик Фаттахов <niaxadeye@outlook.com>
2021-03-13 23:45:14 +0300

    ope

---
+++ 5_6Sh/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_6Sh
+{
+    class Program
+    {
+        public class Dnum
+        {
+            private int num { get; set; }
+            private int den { get; set; }
+           
+            public void
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Lab 1/1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1990          135\n1991                      7290\n1992                      11300\n1993                      16200");
+            Console.ReadKey();
+        }
+    }
+}
